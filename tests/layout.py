e80d5c32e0aa4dda66c424c5806ccf0672586dca
lusamreth <somrethoverkill@gmail.com>
2024-08-05 23:46:36 +0700

    feat: add tests

---
+++ tests/layout.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestLayout(unittest.TestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        pass
+
+    def testFloatingLayout(self):
+        pass
+
+    pass
+
+
+if __name__ == "__main__":
+    TestLayout()
